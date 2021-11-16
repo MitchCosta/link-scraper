0b2d367e8967ec5174cf9893bc5ed684d48c6bcb
Miguel Costa <42335457+MitchCosta@users.noreply.github.com>
2021-11-16 15:46:45 +1100

    Update my_main.py
    prints cleanup

--- my_main.py
+++ my_main.py
@@ -18,13 +18,7 @@ number_per_search = 7
 linkss = linkedin_scraper.set_search(my_list)
 print(linkss)
 
-
 linkedin_scraper.login()
-
-# my_exp = linkedin_scraper.search_profile_fyang()
-# print(my_exp)
-#
-# time.sleep(10)
 
 total_links = linkedin_scraper.search_people(linkss, number_per_search)
 print(f"TOTAL LINKS -> {len(total_links)}")
@@ -41,3 +35,4 @@ print(people_data_dic)
 
 data_frame = pandas.DataFrame.from_dict(people_data_dic, orient='index')
 data_frame.to_csv("people_file.csv")
+
